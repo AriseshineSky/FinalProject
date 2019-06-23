8b4d180620fdd09dc6002ca09f77493d6fe37860
Sky Zhao <befruitful12@gmail.com>
2019-06-22 23:05:40 -0700

    version1

---
+++ Bible/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bible
+//
+//  Created by Sky on 6/14/19.
+//  Copyright © 2019 OU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func createArray()
+    {
+        
+    }
+
+}
+
